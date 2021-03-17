49b3b70785a502d6ea471ce5e771a152aa10a3e6
CruzLiu <liuzhonghe1402@outlook.com>
2021-03-17 21:33:23 +0800

    fix color and ctrl c issue

--- watch.go
+++ watch.go
@@ -1,38 +1,45 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"os/exec"
-	"runtime"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan,
+		syscall.SIGINT,
+	)
 	loc, _ := time.LoadLocation("UTC")
-	fmt.Println("start time: "+time.Now().In(loc).Format(time.RFC3339))
+	fmt.Println("start time: " + time.Now().In(loc).Format(time.RFC3339))
 	argsWithoutProg := os.Args[1:]
 	actualArgsWithoutProg := argsWithoutProg[1:]
 	cmd := exec.Command(argsWithoutProg[0], actualArgsWithoutProg...)
-	stdout, err := cmd.StdoutPipe()
-	cmd.Stderr = cmd.Stdout
-	rd := bufio.NewReader(stdout)
-	if err != nil {
-		return
-	}
-	if err = cmd.Start(); err != nil {
-		return
-	}
-	for {
-		str, err := rd.ReadString('\n')
-		if err != nil {
-			break
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	_ = cmd.Start()
+	go func() {
+		for {
+			s := <-signalChan
+			switch s {
+			// kill -SIGINT XXXX or Ctrl+c
+			case syscall.SIGINT:
+				_ = cmd.Process.Kill()
+			default:
+				fmt.Println("Unknown signal.")
+			}
 		}
-		if runtime.GOOS == "windows" {
-			str = str[:len(str)-1]
-		}
-		fmt.Println(str)
+	}()
+	done := make(chan error, 1)
+	go func() {
+		done <- cmd.Wait()
+	}()
+	select {
+	case <-done:
+		fmt.Println("end time: " + time.Now().In(loc).Format(time.RFC3339))
 	}
-	fmt.Println("end time: "+time.Now().In(loc).Format(time.RFC3339))
 }
